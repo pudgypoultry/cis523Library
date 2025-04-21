cbd3d5441634cfa1e22a6f2f274b2b8bb4c831b5
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-04-20 22:00:13 -0700

    Create library.py

---
+++ library.py
@@ -0,0 +1,9 @@
+from __future__ import annotations  #must be first line in your library!
+import pandas as pd
+import numpy as np
+import types
+from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+import sklearn
+sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
