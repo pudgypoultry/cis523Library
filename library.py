ac835ac231d78423f16d9680478e4c44e231a641
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-04-28 04:44:43 -0700

    Update library.py

--- library.py
+++ library.py
@@ -2,6 +2,7 @@ from __future__ import annotations  #must be first line in your library!
 import pandas as pd
 import numpy as np
 import types
+import warnings
 from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
