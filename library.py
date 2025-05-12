58f1deeb06c43c9864d6ef07a5d4b703b6a4a175
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-05-11 21:57:16 -0700

    Update library.py

--- library.py
+++ library.py
@@ -791,6 +791,35 @@ def find_random_state(
     return rs_value, Var
 
 
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  table_after_y_drop = original_table.drop(columns=label_column_name)
+  labels = original_table[label_column_name].to_list()
+
+  X_train, X_test, y_train, y_test = train_test_split(table_after_y_drop, 
+                                                      labels, 
+                                                      test_size=ts, 
+                                                      shuffle=True,
+                                                      random_state=rs, 
+                                                      stratify=labels)
+  
+  X_train_transformed = the_transformer.fit_transform(X_train, y_train)
+  X_test_transformed = the_transformer.transform(X_test)
+
+  X_train_numpy = X_train_transformed.to_numpy()
+  X_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+  
+  return X_train_numpy, X_test_numpy, y_train_numpy,  y_test_numpy
+
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+  return dataset_setup(titanic_table, 'Survived', transformer, rs, ts=.2)
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+    return dataset_setup(customer_table, 'Rating', transformer, rs, ts=.2)
+
 titanic_variance_based_split = 107
 customer_variance_based_split = 113 
 
